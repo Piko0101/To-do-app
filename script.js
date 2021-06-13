8f1f6c7b26f52760e2b265db2c2e7ba40266a949
Perizat Sabitbek <sonepiko@gmail.com>
2021-06-13 17:59:17 +0600

    write addToDo function

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 
 const list = document.getElementById("list");
+function addToDo(toDo){
+    const text = `<li class="item">
+                        <i class="fa fa-circle-thin complete" job="complete"></i>
+                        <p class="text">${toDo}</p>
+                        <i class="fa fa-trash-o delete" job="delete"></i>
+                  </li>`
+                  
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, text);            
+}
+addToDo("Drink Coffee");
 
 const input = document.getElementById("input");
