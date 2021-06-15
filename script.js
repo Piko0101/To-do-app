8f83c720aac3004dae79b5c8ff389ef8c3871318
Perizat Sabitbek <sonepiko@gmail.com>
2021-06-15 11:18:28 +0600

    improve addToDo function

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const clear = document.querySelector(".clear");
-
 const dateElement = document.getElementById("date");
-
 const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+//classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//show todays date
+const options = {weekday:"long", month: "short", day: "numeric"};
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+//add to do function
 function addToDo(toDo){
-    const text = `<li class="item">
-                        <i class="fa fa-circle-thin complete" job="complete"></i>
-                        <p class="text">${toDo}</p>
-                        <i class="fa fa-trash-o delete" job="delete"></i>
-                  </li>`
-                  
+    const item = `<li class="item">  
+                    <i class="fa fa-circle-thin co" job="complete"></i>
+                    <p class="text">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete"></i>
+                  </li>  `;
     const position = "beforeend";
-    list.insertAdjacentHTML(position, text);            
+    list.insertAdjacentHTML(position, item);            
 }
 addToDo("Drink Coffee");
-
-const input = document.getElementById("input");
